d998070354ca38bf05878f374930b4f7032b48e9
kevin chao <kchao562@gmail.com>
2017-10-30 19:33:42 -0700

    added scrape button

--- server.js
+++ server.js
@@ -64,7 +64,7 @@ app.get("/scrape", function(req, res) {
         .create(result)
         .then(function(dbArticle) {
           // If we were able to successfully scrape and save an Article, send a message to the client
-          res.send("Scrape Complete");
+          res.redirect('/');
         })
         .catch(function(err) {
           // If an error occurred, send it to the client
